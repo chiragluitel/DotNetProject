bc4e5b09e9f2c248951b28a8f01e57a14414c99c
prod-chirag <chiragluitel32@gmail.com>
2026-04-02 18:12:48 +1100

    frontend-backend connected

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,10 +2,34 @@ import SubmitButton from "../components/SubmitButton"
 import { LogIn } from "lucide-react"
 
 const Login = () => {
-    const handleLogin =(formdata: FormData)=>{
-        const email = formdata.get("username")
-        const password = formdata.get("password")
-        console.log ('Submitted Form:', email, password)
+    const handleLogin = async (formdata: FormData)=>{
+        const username = formdata.get("username") as string;
+        const password = formdata.get("password") as string;
+
+        try{
+            const response = await fetch("http://localhost:5035/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body:JSON.stringify({
+                    Username: username,
+                    Password: password
+                })
+            })
+
+            if(response.ok){
+                const text = await response.text();
+                console.log (text)
+            }else if(response.status == 401){
+                const text = await response.text();
+                console.log(text)
+            }else{
+                console.log('Unknown code from backend', response.status)
+            }
+        }catch(error){
+            console.error("Some error: ", error);
+        }
     }
 
     return (
@@ -21,7 +45,7 @@ const Login = () => {
                 <div>
                     <form action={handleLogin} className="flex flex-col items-center gap-4" >
                         <input 
-                        type="email" 
+                        type="text" 
                         name="username" 
                         placeholder="Username" 
                         className="bg-red-200 text-center border-3"
